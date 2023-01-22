611fb0f4e10b5620eac801c945b668991b92f04c
Norbert Matynia <norbert.matyniaa@gmail.com>
2023-01-22 15:41:09 +0000

    feat: clicking outside the buttonSwitch is closing the element

--- src/components/button-switch/ButtonSwitch.tsx
+++ src/components/button-switch/ButtonSwitch.tsx
@@ -1,17 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 type OpenButtonProps = {
   className?: string;
   children: JSX.Element;
   elementToOpen: (open: boolean) => JSX.Element;
 };
-const OpenButton = ({ elementToOpen, children, className }: OpenButtonProps) => {
+const ButtonSwitch = ({ elementToOpen, children, className }: OpenButtonProps) => {
   const [isOpen, setIsOpen] = useState(false);
+
+  const clickOutsideRef = useRef<any>();
+
+  useEffect(() => {
+    const closeDropdown = (e: any) => {
+      if (!clickOutsideRef.current.contains(e.target)) {
+        setIsOpen(false);
+      }
+    };
+    document.body.addEventListener('mousedown', closeDropdown);
+
+    return () => document.body.removeEventListener('mousedown', closeDropdown);
+  }, []);
+
   return (
-    <div className={className}>
+    <div className={className} ref={clickOutsideRef}>
       <div onClick={() => setIsOpen(!isOpen)}>{children}</div>
       {elementToOpen(isOpen)}
     </div>
   );
 };
 
-export default OpenButton;
+export default ButtonSwitch;
