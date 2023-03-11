1b5b170752a96c7a0366c9f024afc6bdb3a62052
Norbert Matynia <norbert.matyniaa@gmail.com>
2023-03-11 18:23:46 +0000

    feat: decoupled the form into seperate file

--- src/components/item-creation-dialog/ItemCreationDialog.tsx
+++ src/components/item-creation-dialog/ItemCreationDialog.tsx
@@ -8,56 +8,14 @@ import { Form } from '../forms/Form';
 import { InputField } from '../forms/InputField';
 import { SelectField } from '../forms/SelectField';
 import { BodyText } from '../typography/Typography';
+import { ItemInfoForm } from './forms/ItemInfoForm';
 type ItemCreationDialogProps = {
   isOpen: boolean;
   handleCloseDialog: () => void;
 };
-export const ItemDetailsSchema = z.object({
-  name: z.string(),
-  brand: z.string(),
-  size: z.any()
-  // object({ id: z.string(), name: z.string() })
-});
-
-type ItemDetailsType = z.infer<typeof ItemDetailsSchema>;
-
-const sizeOptions = [
-  { name: 'XS', id: 'XS' },
-  { name: 'S', id: 'S' },
-  { name: 'M', id: 'M' },
-  { name: 'L', id: 'L' },
-  { name: 'XL', id: 'XL' },
-  { name: 'XXL', id: 'XXL' }
-];
 
 export const ItemCreationDialog = ({ isOpen, handleCloseDialog }: ItemCreationDialogProps) => {
-  const methods = useForm<ItemDetailsType>({
-    resolver: zodResolver(ItemDetailsSchema),
-    defaultValues: {
-      size: sizeOptions[0],
-      name: undefined,
-      brand: undefined
-    }
-  });
-
-  const {
-    register,
-    watch,
-    handleSubmit,
-    reset,
-    formState: { errors, isSubmitting }
-  } = methods;
-
-  const onSubmit: SubmitHandler<ItemDetailsType> = async (data, e) => {
-    handleCloseDialog();
-    e?.preventDefault();
-    console.log(data);
-    // await updateUser(data);
-    // handleDisableEditing();
-  };
-
   return (
-    //isOpen
     <Transition appear show={isOpen} as={Fragment}>
       <Dialog as="div" className="relative z-10" onClose={handleCloseDialog}>
         <Transition.Child
@@ -87,42 +45,7 @@ export const ItemCreationDialog = ({ isOpen, handleCloseDialog }: ItemCreationDi
                 <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
                   Add a new item
                 </Dialog.Title>
-                <Form onSubmit={handleSubmit(onSubmit)} {...methods}>
-                  <div className="mt-7 flex flex-col gap-8">
-                    <div className="flex w-full flex-col gap-6 md:flex-row">
-                      <div className="w-1/2">
-                        <BodyText>Brand:</BodyText>
-                        <InputField
-                          placeholder={"Enter item's brand"}
-                          name="brand"
-                          className="w-full"
-                        />
-                      </div>
-                      <div className="w-1/2">
-                        <BodyText>Name:</BodyText>
-                        <InputField
-                          placeholder={"Enter item's name"}
-                          name="name"
-                          className="w-full"
-                        />
-                      </div>
-                    </div>
-                    <div className="flex w-full flex-col gap-6 md:flex-row">
-                      <SelectField
-                        name="size"
-                        label="Size"
-                        className="w-full"
-                        options={sizeOptions}
-                      />
-                    </div>
-                  </div>
-                  <div className="mt-4 flex flex-row-reverse justify-start gap-3">
-                    <Button type="submit">Add item</Button>
-                    <Button variant="outlined" onClick={handleCloseDialog}>
-                      Cancel
-                    </Button>
-                  </div>
-                </Form>
+                <ItemInfoForm handleCloseDialog={handleCloseDialog} />
               </Dialog.Panel>
             </Transition.Child>
           </div>
