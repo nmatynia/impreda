e0c72c6a57b303de92e62b6b79b1c7dc76b67a48
Norbert Matynia <norbert.matyniaa@gmail.com>
2023-03-11 16:15:23 +0000

    feat: logout button only displayed when logged in

--- src/components/navbar/header/account-menu/AccountMenu.tsx
+++ src/components/navbar/header/account-menu/AccountMenu.tsx
@@ -1,56 +1,55 @@
-import { signOut } from 'next-auth/react';
+import { GetServerSideProps } from 'next';
+import { getSession, signOut, useSession } from 'next-auth/react';
 import Link from 'next/link';
-import React from 'react';
+import React, { ReactNode } from 'react';
+import { getServerAuthSession } from '../../../../server/common/get-server-auth-session';
 import clsxm from '../../../../utils/clsxm';
 import Box, { BoxProps } from '../../../box/Box';
 import { IconName, SvgIcon } from '../../../icons/SvgIcon';
 import { BodyText } from '../../../typography/Typography';
 type AccountMenuProps = {
   className?: string;
+  session: boolean;
 } & BoxProps;
 
-type MenuItemProps = {
-  icon: IconName;
-  text: string;
-  path?: string;
-  onClick?: () => void;
-};
-
-const menuItems: MenuItemProps[] = [
-  {
-    icon: 'OutlinedPerson',
-    text: 'Account',
-    path: '/account'
-  },
-  {
-    icon: 'OutlinedSettings',
-    text: 'Preferences'
-  },
-  {
-    icon: 'Logout',
-    text: 'Logout',
-    onClick: () => signOut()
-  }
-];
 const AccountMenu = ({ className, ...props }: AccountMenuProps) => {
+  const { data: session } = useSession();
   return (
     <Box className={clsxm('absolute', className)} {...props}>
-      {menuItems.map(item => (
-        <div className="flex cursor-pointer select-none items-center gap-4 border-b-[1px] border-primaryBlack py-4 first:pt-0 last:border-0 last:pb-0">
-          {item.onClick ? (
-            <button className="flex items-center justify-center gap-4" onClick={item.onClick}>
-              <SvgIcon name={item.icon} />
-              <BodyText>{item.text}</BodyText>
-            </button>
-          ) : (
-            <Link href={item.path || '/'} className="flex items-center justify-center gap-4">
-              <SvgIcon name={item.icon} />
-              <BodyText>{item.text}</BodyText>
-            </Link>
-          )}
-        </div>
-      ))}
+      <ItemWrappper>
+        <Link href={'/account'} className="flex items-center justify-center gap-4">
+          <SvgIcon name="OutlinedPerson" />
+          <BodyText>Account</BodyText>
+        </Link>
+      </ItemWrappper>
+      <ItemWrappper>
+        <Link href={'/'} className="flex items-center justify-center gap-4">
+          <SvgIcon name="OutlinedSettings" />
+          <BodyText>Preferences</BodyText>
+        </Link>
+      </ItemWrappper>
+      {session && (
+        <ItemWrappper>
+          <button className="flex items-center justify-center gap-4" onClick={() => signOut()}>
+            <SvgIcon name="Logout" />
+            <BodyText>Logout</BodyText>
+          </button>
+        </ItemWrappper>
+      )}
     </Box>
+  );
+};
+
+export const ItemWrappper = ({ children }: { children: ReactNode }) => {
+  return (
+    <div
+      className={clsxm(
+        'flex cursor-pointer select-none items-center gap-4 border-b-[1px]',
+        'border-primaryBlack py-4 first:pt-0 last:border-0 last:pb-0'
+      )}
+    >
+      {children}
+    </div>
   );
 };
 
