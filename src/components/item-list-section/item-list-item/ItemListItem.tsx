f4cd490d619cf12f3df5b6c22deec64943096bfc
Norbert Matynia <norbert.matyniaa@gmail.com>
2023-03-25 19:26:02 +0000

    fix: uneven stats preview in item list

--- src/components/item-list-section/item-list-item/ItemListItem.tsx
+++ src/components/item-list-section/item-list-item/ItemListItem.tsx
@@ -19,22 +19,22 @@ export const ItemListItem = ({ className, item }: ItemListItemProps) => {
         className
       )}
     >
-      <div className="flex w-full max-w-[50%] gap-4 sm:max-w-max">
+      <div className="flex w-[calc(100%-20px)] basis-full  items-center gap-4 overflow-hidden">
         <div className="relative aspect-square h-[52px] bg-primaryWhite">
           <Image src={item.images[0].url ?? ''} fill className="object-contain" alt="Item Image" />
         </div>
-        <div className="flex w-full flex-col gap-3">
+        <div className="flex w-[calc(100%-68px)] basis-full flex-col gap-3">
           <BodyText className="w-full overflow-hidden overflow-ellipsis whitespace-nowrap ">
             {item.brand} {item.name}
           </BodyText>
-          <div className="flex w-fit gap-4 whitespace-pre sm:gap-24">
+          <div className="grid w-full whitespace-pre xs:grid-cols-2 sm:grid-cols-3">
             <BodyText>Price: {item.price}£</BodyText>
             <BodyText>Views: {item.views}</BodyText>
             <BodyText className="hidden sm:block">Favorites: {item.savedBy}</BodyText>
           </div>
         </div>
       </div>
-      <button type="button" className="cursor-pointer">
+      <button type="button" className="basis-5 cursor-pointer">
         <SvgIcon name="Edit" className="ml-5 fill-primaryWhite" />
       </button>
     </RoundedBox>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    screens: {
+      xs: '375px',
+      ...defaultTheme.screens
+    },
     extend: {
       colors: {
         primaryBlack: '#171717',
